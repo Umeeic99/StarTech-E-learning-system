7c486e3c8d94d7f93c160e470a5336e676da202d
UMAR KHALID <128242053+Umeeic99@users.noreply.github.com>
2023-07-30 17:28:38 +0500

    Update config1.php

--- config1.php
+++ config1.php
@@ -1,11 +1,1 @@
-<?php
-require('stripe-php-master/init.php');
 
-$publishableKey=
-"pk_test_51JfUdxE7o9QwvWvs82U9u42Ej91ArS46nsLLREjkbmEZZo9oqMEghIU7wopP6kMfahEYQcL2YjVt0IoPxEkvzZXN00SNMDTZSw";
-
-$secretKey=
-"sk_test_51JfUdxE7o9QwvWvs2hUtkMpihgpwutQIRCS8JI0WYJYlUFpKdiEeLMej2WXHInQ12ZVQwqTUPrunjMLMAm8j8Olr00wi6kb1ul";
-
-\Stripe\Stripe::setApiKey($secretKey);
-?>
